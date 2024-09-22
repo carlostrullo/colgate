3507ac3bc473a965de76769a7f9d906ef1434479
JRFIGITAL <carlos.2396@hotmail.com>
2024-09-22 13:11:30 -0500

    Update download.py

--- download.py
+++ download.py
@@ -3,6 +3,8 @@ import gdown
 # URL del archivo de Google Drive
 url = 'https://drive.google.com/uc?id=1emEBEK2_Vnd4bntMcsEoAnhX8VF2Z3xr'
 
+# https://drive.google.com/file/d/1emEBEK2_Vnd4bntMcsEoAnhX8VF2Z3xr/view
+
 # Nombre con el que se guardará el archivo descargado
 output = 'video.mp4'
 
